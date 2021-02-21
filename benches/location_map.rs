afef186079b6103acc18590c59c2eec5c198b0d6
MrInformatic <maxundmoritz99199@gmail.com>
2021-02-21 19:53:53 +0100

    Add location_map benchmark

---
+++ benches/location_map.rs
@@ -0,0 +1,43 @@
+use std::mem::MaybeUninit;
+
+use criterion::*;
+use legion::*;
+use rand::{prelude::SliceRandom, thread_rng};
+use world::{Allocate, LocationMap};
+
+const ENTITY_COUNTS: [usize; 5] = [1_000, 10_000, 100_000, 1_000_000, 10_000_000];
+const QUERY_COUNT: usize = 1_000_000;
+
+fn bench_location_map(criterion: &mut Criterion) {
+    for entity_count  in &ENTITY_COUNTS {
+        let mut location_map = LocationMap::default();
+
+        let allocate = Allocate::new();
+
+        let entities = allocate.take(*entity_count).collect::<Vec<_>>();
+        
+        for entity in &entities {
+            location_map.set(*entity, unsafe { MaybeUninit::uninit().assume_init() });
+        }
+
+        let mut rng = thread_rng();
+
+        let choosen_entites = entities.choose_multiple(&mut rng, QUERY_COUNT)
+            .cloned()    
+            .collect::<Vec<_>>();
+        
+        criterion.bench_with_input(BenchmarkId::new("bench_location_map", entity_count), entity_count, |bencher, _| {
+            bencher.iter(|| {
+                for entity in &choosen_entites {
+                    location_map.get(*entity);
+                }
+            });
+        });       
+    }
+}
+
+criterion_group!(
+    basic,
+    bench_location_map
+);
+criterion_main!(basic);
